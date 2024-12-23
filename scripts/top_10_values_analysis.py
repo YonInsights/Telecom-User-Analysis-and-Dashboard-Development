3f0047c50ab02e20de51459406dd77e3a44c9ee7
Yoninsight <yo2120018@gmail.com>
2024-12-23 10:13:51 +0300

    most_frequent_values

--- scripts/top_10_values_analysis.py
+++ scripts/top_10_values_analysis.py
@@ -26,6 +26,22 @@ def bottom_10_values(df, column_name):
     pd.Series: The bottom 10 smallest values from the column.
     """
     return df[column_name].nsmallest(10)
+# Function to compute the most frequent values (mode) for a given column
+def c(df, column_name):
+    """
+    Returns the most frequent values (mode) from the specified column of the DataFrame.
+    
+    Parameters:
+    df (pd.DataFrame): The DataFrame containing the data.
+    column_name (str): The name of the column to analyze.
+    
+    Returns:
+    pd.Series: The most frequent values (mode) from the column.
+    """
+    return df[column_name].mode()
+
+
+
 
 
 
